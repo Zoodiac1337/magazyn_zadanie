a80faac06f5f210db124926e828a0a501ea72375
Pawel <pawelx14@gmail.com>
2026-07-12 20:53:40 +0200

    Limited number of failed login attempts and added TestUserController to manually add first users.

---
+++ src/Controller/TestUsersController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+
+class TestUsersController
+{
+    #[Route('/test-users', name: 'app_test_users')]
+    public function index(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    {
+        // regular user
+        $user = new User();
+        $user->setUsername('user');
+        $user->setRoles(['ROLE_USER']);
+        $hashedUserPassword = $passwordHasher->hashPassword($user, 'user123');
+        $user->setPassword($hashedUserPassword);
+
+        // admin user
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $hashedAdminPassword = $passwordHasher->hashPassword($admin, 'admin123');
+        $admin->setPassword($hashedAdminPassword);
+
+        // persist users to the database
+        $entityManager->persist($user);
+        $entityManager->persist($admin);
+        // flush changes to the database
+        $entityManager->flush();
+
+        return new Response('Test users created successfully.');
+    }
+}
