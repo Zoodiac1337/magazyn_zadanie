7b954b6e62fb0fe5e20063f4e1a5eb6e8137cd0e
Pawel <pawelx14@gmail.com>
2026-07-14 21:27:59 +0200

    Added twig templates for issue and receive goods with revised css style in app.css (and placeholders for the rest of the pages)

--- src/Controller/WarehouseController.php
+++ src/Controller/WarehouseController.php
@@ -14,4 +14,39 @@ class WarehouseController extends AbstractController
         return $this->render('warehouse.html.twig');
 
     }
+
+    #[Route('/receive-goods', name: 'app_receive_goods')]
+    public function receiveGoods(): Response
+    {
+        return $this->render('receivegoods.html.twig');
+
+    }
+
+    #[Route('/issue-goods', name: 'app_issue_goods')]
+    public function issueGoods(): Response
+    {
+        return $this->render('issuegoods.html.twig');
+
+    }
+
+    #[Route('/manage-warehouses', name: 'app_manage_warehouses')]
+    public function manageWarehouses(): Response
+    {
+        return $this->render('managewarehouses.html.twig');
+
+    }
+
+    #[Route('/manage-users', name: 'app_manage_users')]
+    public function manageUsers(): Response
+    {
+        return $this->render('manageusers.html.twig');
+
+    }
+
+    #[Route('/manage-items', name: 'app_manage_items')]
+    public function manageItems(): Response
+    {
+        return $this->render('manageitems.html.twig');
+
+    }
 }
